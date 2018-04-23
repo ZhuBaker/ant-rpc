883b8b805666bea9de938b8ccfa6213c127ded07
zhubo <zhubo@58qf.com>
2018-04-23 21:38:58 +0800

    通讯模块开发

---
+++ ant-client/src/main/java/com/antrpc/client/App.java
@@ -0,0 +1,13 @@
+package com.antrpc.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
